e4688a500b24d72f745817e1141f4fcb4b8d4bd5
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-12 09:47:48 -0300

    Criando os imports para o express

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const transactionRouter = express.Router();
 
 module.exports = transactionRouter;
--- services/transactionService.js
+++ services/transactionService.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from'mongoose';
 const ObjectId = mongoose.Types.ObjectId;
 
 // Aqui havia um erro difícil de pegar. Importei como "transactionModel",
