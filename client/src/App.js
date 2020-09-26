64811543cae6b1b13558063cbd1281102e1fe095
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-26 13:02:32 -0300

    Criando os states

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const api = axios.create({ baseURL: 'api' });
+
 
 export default function App() {
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+  const [transactions, setTransections] = useState([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      const { data } = await api.get('/transaction?period=2019-07');
+      setTransections(data);
+    };
+    fetchTransactions();
+  }, []);
+  console.log(transactions);
+  return (
+    <h1>Desafio Final do Bootcamp Full Stack</h1>
+  );
 }
