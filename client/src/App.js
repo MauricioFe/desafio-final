dc3bd1b4c790b43c4d673ae2fa607424f58d7069
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-26 15:10:31 -0300

    Configurando forma de mostrar as transacoes

--- client/src/App.js
+++ client/src/App.js
@@ -2,70 +2,94 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 const api = axios.create({ baseURL: 'api' });
 const PERIODS = [
-  '2019/01',
-  '2019/02',
-  '2019/03',
-  '2019/04',
-  '2019/05',
-  '2019/06',
-  '2019/07',
-  '2019/08',
-  '2019/09',
-  '2019/10',
-  '2019/11',
-  '2019/12',
-  '2020/01',
-  '2020/02',
-  '2020/03',
-  '2020/04',
-  '2020/05',
-  '2020/06',
-  '2020/07',
-  '2020/08',
-  '2020/09',
-  '2020/10',
-  '2020/11',
-  '2020/12',
-  '2021/01',
-  '2021/02',
-  '2021/03',
-  '2021/04',
-  '2021/05',
-  '2021/06',
-  '2021/07',
-  '2021/08',
-  '2021/09',
-  '2021/10',
-  '2021/11',
-  '2021/12',
+  '2019-01',
+  '2019-02',
+  '2019-03',
+  '2019-04',
+  '2019-05',
+  '2019-06',
+  '2019-07',
+  '2019-08',
+  '2019-09',
+  '2019-10',
+  '2019-11',
+  '2019-12',
+  '2020-01',
+  '2020-02',
+  '2020-03',
+  '2020-04',
+  '2020-05',
+  '2020-06',
+  '2020-07',
+  '2020-08',
+  '2020-09',
+  '2020-10',
+  '2020-11',
+  '2020-12',
+  '2021-01',
+  '2021-02',
+  '2021-03',
+  '2021-04',
+  '2021-05',
+  '2021-06',
+  '2021-07',
+  '2021-08',
+  '2021-09',
+  '2021-10',
+  '2021-11',
+  '2021-12',
 ]
+
+const LIST_SCREEN = 0;
+const MAINTENANCE_SCREEN = 1;
+const EXPENSE_COLOR = '#fc5c65';
+const EARNING_COLOR = '#0fb9b1';
+
 export default function App() {
   const [transactions, setTransections] = useState([]);
   const [filteredTransactions, setFilteredTransactions] = useState([]);
   const [currentPeriod, setCurrentPeriod] = useState(PERIODS[0]);
+  const [currentScreen, setCurrentScreem] = useState(LIST_SCREEN);
 
   useEffect(() => {
     const fetchTransactions = async () => {
-      const { data } = await api.get('/transaction?period=2019-07');
+      const { data } = await api.get(`/transaction?period=${currentPeriod}`);
       setTransections(data.transactions);
       setFilteredTransactions(data.transactions);
     };
     fetchTransactions();
-  }, []);
+  }, [currentPeriod]);
+
+
   const handlePeriodChenge = (event) => {
     setCurrentPeriod(event.target.value);
   }
   return (
     <div className="container">
       <h1 className="center">Desafio Final do Bootcamp Full Stack</h1>
-      <select className="browser-default" value={currentPeriod} onChange={handlePeriodChenge}>{
-        PERIODS.map(period => {
-          return <option key={period}>{period}</option>
-        })
-      }</select>
-      {filteredTransactions.map(transaction => {
-        return <p key={transaction._id}>{transaction.description}</p>
-      })}
+      {
+        currentScreen === LIST_SCREEN ?
+          <>
+            <select className="browser-default" value={currentPeriod} onChange={handlePeriodChenge}>{
+              PERIODS.map(period => {
+                return <option key={period}>{period}</option>
+              })
+            }</select>
+            {filteredTransactions.map(transaction => {
+              const currentColor = transaction.type == '+' ? EARNING_COLOR : EXPENSE_COLOR;
+              return (
+                <div className="card" style={{ marginBttom: '8px', padding: "8px", backgroundColor: currentColor }}>
+                  <p key={transaction._id}>
+                    {transaction.yearMonthDay} {' - '}
+                    <strong>{transaction.category}</strong> {' '}
+                    {transaction.description} - {transaction.value}
+                  </p>
+                </div>
+              )
+            })}
+          </>
+          : <p>Tela de manutenção</p>
+      }
     </div>
   );
 }
